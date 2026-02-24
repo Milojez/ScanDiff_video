dd6cd8ded3f5ce230314720e48b395ddf7535713
Milosz <miloszjezierski1@gmail.com>
2026-02-24 03:02:16 +0100

    changes to train_ave.sh

--- jobs/train_ave.sh
+++ jobs/train_ave.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-python src/train.py -m data/train_datasets=[coco_freeview,mit1003] \
-    data/val_datasets=[mit1003] \
-    data/test_datasets=[coco_freeview] \
+python src/train.py -m data/train_datasets=[yke_data] \
+    data/val_datasets=[yke_data] \
+    data/test_datasets=[yke_data] \
     trainer=gpu diffusion.num_timesteps=1000 \
     data.num_workers=8 callbacks=default \
     logger=wandb slurm=null \
-    tags=[scandiff_training_freeview] \
-    trainer.validation_every_n_epochs=10 \
-    trainer.test_every_n_epochs=10 \
-    trainer.max_epochs=200 \
+    tags=[ykedata_training_2s_1500mHz] \
+    trainer.validation_every_n_epochs=1 \
+    trainer.test_every_n_epochs=1 \
+    trainer.max_epochs=1 \
     train=true test=true \
     evaluation.data_to_extract=['preds','metrics','qualitatives'] \
     evaluation.metrics_to_compute=['multi_match','scan_match','scan_match_no_dur','sequence_score','sequence_score_time','kld','diversity_sequence_score','diversity_sequence_score_time'] \
     diffusion_class=spaced_diffusion \
-    callbacks.model_checkpoint.every_n_epochs=10 \
-    ckpt_path=null \
+    callbacks.model_checkpoint.every_n_epochs=1 \
+    ckpt_path= checkpoints/scandiff_freeview.pth\
