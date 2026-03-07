53377f961cf472d7b47a0077676c66dfbc310c3a
Milosz <miloszjezierski1@gmail.com>
2026-03-07 19:17:48 +0100

    changing the number of batches for more stable updates of batchnorms stats

--- jobs/train_ave.sh
+++ jobs/train_ave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # evaluation.data_to_extract=['preds','metrics','qualitatives'] \
 # evaluation.metrics_to_compute=['multi_match','scan_match','scan_match_no_dur','sequence_score','sequence_score_time','kld','diversity_sequence_score','diversity_sequence_score_time']
-#what is callbacks.model_checkpoint.every_n_epochs=10 \
+#what is callbacks.model_checkpoint.every_n_epochs=10 - savesa checpoint every n epochs
 python src/train.py -m data/train_datasets=[ykedata] \
     data/val_datasets=[ykedata] \
     data/test_datasets=[ykedata] \
@@ -16,6 +16,6 @@ python src/train.py -m data/train_datasets=[ykedata] \
     evaluation.data_to_extract=['preds','metrics','qualitatives'] \
     evaluation.metrics_to_compute=['multi_match','scan_match','scan_match_no_dur','kld'] \
     diffusion_class=spaced_diffusion \
-    callbacks.model_checkpoint.every_n_epochs=1 \
+    callbacks.model_checkpoint.every_n_epochs=10 \
     ckpt_path="./checkpoints/scandiff_freeview.pth" \
 
