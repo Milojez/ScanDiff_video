6db1d6c680a1f662cbb48dbaccdbc2edfd137b03
Milosz <miloszjezierski1@gmail.com>
2026-03-07 18:56:17 +0100

    addign a  trainer script now with possibility to freeze the batches

--- jobs/train_ave.sh
+++ jobs/train_ave.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # evaluation.data_to_extract=['preds','metrics','qualitatives'] \
 # evaluation.metrics_to_compute=['multi_match','scan_match','scan_match_no_dur','sequence_score','sequence_score_time','kld','diversity_sequence_score','diversity_sequence_score_time']
+#what is callbacks.model_checkpoint.every_n_epochs=10 \
 python src/train.py -m data/train_datasets=[ykedata] \
     data/val_datasets=[ykedata] \
     data/test_datasets=[ykedata] \
@@ -8,9 +9,9 @@ python src/train.py -m data/train_datasets=[ykedata] \
     data.num_workers=8 callbacks=default \
     logger=wandb ~slurm \
     tags=[ykedata_training_2s_1500mHz] \
-    trainer.validation_every_n_epochs=1 \
+    trainer.validation_every_n_epochs=5 \
     trainer.test_every_n_epochs=9999 \
-    trainer.max_epochs=1 \
+    trainer.max_epochs=5 \
     train=true test=true \
     evaluation.data_to_extract=['preds','metrics','qualitatives'] \
     evaluation.metrics_to_compute=['multi_match','scan_match','scan_match_no_dur','kld'] \
